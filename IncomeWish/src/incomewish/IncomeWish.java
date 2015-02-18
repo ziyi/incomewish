a66dd3a84c0afa0312bd8b383313ec5194a9bafa
class <class@CST_N417_22.cst.citytech.cuny.edu>
2015-02-18 17:02:00 -0500

    IncomeWish

---
+++ IncomeWish/src/incomewish/IncomeWish.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package incomewish;
+
+/**
+ *
+ * @author class
+ */
+public class IncomeWish {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String name;
+    int age;
+    double annualPay;
+    name = "Joe Mahoney";
+    age = 26;
+    annualPay = 100000;
+    System.out.println("My name is " + name + ". " + "My age is " + age + " and " 
+            + "I hope to earn " + annualPay + " per year.");
+    }
+    
+}
